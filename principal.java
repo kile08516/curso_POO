f47c91fa99b47419bdd438fa2cd568cad020572b
kile08516 <kile08516@gmail.com>
2022-08-16 18:50:03 -0500

    actualizado

--- principal.java
+++ principal.java
@@ -1,29 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package herencia;
+package ATM;
 
-/**
- *
- * @author TECNOLOGICO
- */
-public class principal {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic herez
-        bebidas crear_bebidas = new bebidas( "COCA", "LATA", "COCA COLA", "B01", 15, "COCA-COLA", "BEBIDA");{
-        
-        System.out.println("Nombre Producto:" +crear_bebidas.getNombre());
-        System.out.println("Marca Producto:" +crear_bebidas.getMarca());
-        System.out.println("Sabor Producto:" +crear_bebidas.getSabor());
-    }
-   }
+
+public class Principal {
+
+	public static void main(String[] args) {
+	
+	Plantilla creando_cajero= new Consultar_saldo();
+	
+		creando_cajero.setSaldo(500);
+		creando_cajero.Operaciones();
+	}
 }
-        
-                
-
